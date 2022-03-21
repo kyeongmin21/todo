73cbf26216a51d5cfbc14242aefb600992d201af
kim kyeong min <kyeongmin21@naver.com>
2022-03-21 20:57:13 +0900

    app 페이지, bootstrap.css 링크 연결

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
-  <div id="app">
-
-    <router-view/>
+  <div id="app" class="container">
+    <h1 class="text-center">Todo App</h1>
+    <input type="text" class="w-100 p-2" placeholder="Type todo">
+    <hr>
+    <div>
+      <input type="checkbox">
+      <span class="ml-3">Buy a car</span>
+    </div>
   </div>
 </template>
