87abbe16f078fbe92d3526a28db3270e0b77b45b
kim kyeong min <kyeongmin21@naver.com>
2022-03-21 21:04:06 +0900

    Todo 컴포넌트 v-for문

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <h1 class="text-center">Todo App</h1>
     <input type="text" class="w-100 p-2" placeholder="Type todo">
     <hr>
-    <Todo></Todo>
+    <Todo v-for="todo in todos" :key="todo.id"></Todo>
   </div>
 </template>
 <script>
@@ -11,6 +11,14 @@ import Todo from '@/components/Todo'
 export default {
   components: {
     Todo
+  },
+  data() {
+    return {
+      todos: [
+        { id: 1, text: 'buy a car', checked: false },
+        { id: 2, text: 'play game', checked: false }
+      ]
+    }
   }
 }
 </script>
