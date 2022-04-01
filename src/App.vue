842e31bee7c0c6cfd1f4cfdf217b5ffd988b4902
kim kyeong min <kyeongmin21@naver.com>
2022-04-01 22:29:36 +0900

    체크박스 부분을 Todo 컴포넌트로 만들고 App.vue랑 연결시키기

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <div id="app" class="container">
-
+    <h1 class="text-center">Todo App</h1>
+    <input type="text" class="w-100 p-2">
+    <hr>
+    <Todo></Todo>
   </div>
 </template>
 <script>
-
-
+import Todo from "@/components/Todo"
 export default {
-  components: {},
+  components: {Todo},
   data() {
     return {}
   },
--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
-
+    <input type="checkbox"><span class="ml-3">Buy a car</span>
   </div>
 </template>
 
 <script>
 export default {
   name: "Todo",
+  props: {
 
-  methods: {
-
-    },
+  },
+  methods: {},
 
 }
 </script>
