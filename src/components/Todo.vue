81c08b43724dfd802ff66ae95d8eedefef40a264
kim kyeong min <kyeongmin21@naver.com>
2022-04-03 23:29:33 +0900

    clickDelete 이벤트

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,14 +1,20 @@
 <template>
-  <div>
-    {{ todoList.checked}}
-    <input type="checkbox"
-           :checked="todoList.checked"
-           @change="toggleCheckbox">
-    <span class="ml-3"
+  <div class="mb-2 d-flex">
+    <div>
+      <input type="checkbox"
+             :checked="todoList.checked"
+             @change="toggleCheckbox">
+    </div>
+
+    <span class="ml-3 flex-grow-1"
           :class="todoList.checked ? 'text-muted' : ''"
           :style="todoList.checked ? 'text-decoration:line-through' : ''">
       {{ todoList.text }}
     </span>
+    <button class="btn btn-danger btn-sm"
+            @click="clickDelete">
+      delete
+    </button>
   </div>
 </template>
 
@@ -27,6 +33,9 @@ export default {
         id: this.todoList.id,
         checked: e.target.checked
       })
+    },
+    clickDelete() {
+      this.$emit('click-delete', this.todo.id)
     }
   }
 }
