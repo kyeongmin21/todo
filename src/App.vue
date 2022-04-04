bb7bd9a6d74df98a87dda360e4db7d5b17add53c
kim kyeong min <kyeongmin21@naver.com>
2022-04-04 19:59:06 +0900

    복습

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,38 @@
 <template>
   <div id="app" class="container">
     <h1 class="text-center">Todo App</h1>
-    <input type="text"
-           v-model="clearText"
-           class="w-100 p-2"
-           placeholder="Type todo"
-           @keyup.enter="addTodo">
+    <input type="text" class="w-100 p-2"
+           placeholder="todo list"
+           @keyup.enter="addTodo"
+           v-model="cleanText">
     <hr>
     <Todo v-for="todo in todos"
           :key="todo.id"
-          :todoList="todo"
-          @toggle-checkbox="toggleCheckbox"
-          @click-delete="clickDelete">
-    </Todo>
+          :todoList="todo"></Todo>
   </div>
 </template>
 
 <script>
-import Todo from "@/components/Todo"
+import Todo from '@/components/Todo'
 export default {
   components: {Todo},
   data() {
     return {
-      clearText: '',
+      cleanText: '',
       todos: [
-        {id: 1, text: 'study hard', checked: true},
-        {id: 2, text: 'buy a car', checked: false},
+        { id: 1, text: 'study hard', checked: true} ,
+        { id: 2, text: 'buy a car', checked: false },
       ]
     }
   },
   methods: {
-    addTodo(e) {
+    addTodo(event) {
       this.todos.push(
-      {id: Math.random(),
-        text: e.target.value,
-        checked: false}
-      );
-      this.clearText = ''
-    },
-    toggleCheckbox({id, checked}) {
-      const index = this.todos.findIndex( todo => {
-        return todo.id === id;
-      })
-      this.todos[index].checked = checked;
-    },
-    clickDelete(id) {
-      // const index = this.todos.findIndex( todo => {
-      //   return todo.id === id
-      // });
-      // this.todos.splice(index, 1)
-      this.todos = this.todos.filter(todo => todo.id !== id)
+      { id: Math.random(), text: event.target.value, checked: false }
+      )
+      this.cleanText =''
     }
+
   }
 }
 </script>
--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,19 +1,9 @@
 <template>
-  <div class="mb-2 d-flex">
-    <div>
-      <input type="checkbox"
-             :checked="todoList.checked"
-             @change="toggleCheckbox">
-    </div>
-
-    <span class="ml-3 flex-grow-1"
-          :class="todoList.checked ? 'text-muted' : ''"
-          :style="todoList.checked ? 'text-decoration:line-through' : ''">
-      {{ todoList.text }}
-    </span>
-    <button class="btn btn-danger btn-sm"
-            @click="clickDelete">delete
-    </button>
+  <div>
+    {{ todoList.checked}}
+    <input type="checkbox"
+           :checked="todoList.checked">
+    <span class="ml-3">{{ todoList.text }}</span>
   </div>
 </template>
 
@@ -23,19 +13,10 @@ export default {
   props: {
     todoList: {
       type: Object,
-      required: true,
     }
   },
   methods: {
-    toggleCheckbox(e) {
-      this.$emit('toggle-checkbox', {
-        id: this.todoList.id,
-        checked: e.target.checked
-      })
-    },
-    clickDelete() {
-      this.$emit('click-delete', this.todoList.id)
-    }
+
   }
 }
 </script>
